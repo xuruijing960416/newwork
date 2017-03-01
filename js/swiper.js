5a5ff24142277283bd83d80c41e35e9dc557a55e
xuruijing960416 <1757059101@qq.com>
2017-03-01 10:33:57 +0800

    git init

---
+++ js/swiper.js
@@ -0,0 +1,22 @@
+
+(function(){
+		    	function changRem(){
+		    		var scale = 1 / devicePixelRatio;  
+		            document.querySelector('meta[name="viewport"]').setAttribute('content',
+		            'width=device-width,initial-scale=' + scale + ',maximum-scale=' + scale + 
+		            ', minimum-scale=' + scale + ', user-scalable=no');
+		            //flexible中定义<html>font-size
+					var width = document.documentElement.getBoundingClientRect().width;  
+					if (width / devicePixelRatio > 540) {  
+					    width = 540 * devicePixelRatio;
+					}
+					var rem = width / 10;
+					document.documentElement.style.fontSize = rem + 'px';
+		    	}
+		    	changRem();
+		    	var timer;
+		    	window.addEventListener("resize",function(){
+		    		clearInterval(timer);
+		    		timer = setInterval(changRem,10);
+		    	})
+		    })()
